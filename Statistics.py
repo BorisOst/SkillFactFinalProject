b0cbd37c72d1c813cf5d54b88ce8c7ea990f58c8
b.ostreshko <b.ostreshko@1440.space>
2023-03-13 00:34:35 +0300

    - добавлены Statistics.py + exp03

--- Statistics.py
+++ Statistics.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 
 class Statistics():
     """Классс для работы со статистиками и ошибками """
@@ -21,8 +21,14 @@ class Statistics():
         mse = mean_squared_error(self.actuals, self.predictions)
         return mse
 
+    def mape(self):
+        mape = mean_absolute_percentage_error(
+            self.actuals, self.predictions)
+        return mape
+
     def report(self):
         print(f"MAE: {self.mae:.2f}")
         print(f"MSE: {self.mse:.2f}")
         print(f"MRSE: {np.sqrt(self.mse):.2f}")
+        print(f"MAPE: {self.mape:.2f}")
         
